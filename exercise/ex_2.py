1242d6c48bccd5c67b0b678e1d90b3eef38145c0
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-06 20:25:42 +0200

    dodany trapez

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,5 +1,9 @@
-#prostokat
-a = 10
-b =5
-obwód  = a * 2 + 2 * b
-pole = a * b
+# trapez
+a = 3
+b = 4
+c = 2
+h = 5
+obwod  = 2* a + 2*4 + 2*c
+pole = (a+b)*h/2
+print ("obwod trapezu wynosi" + str(obwod) + ", pole" + str(pole)+".")
+print(pole)
