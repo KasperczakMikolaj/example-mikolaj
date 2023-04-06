2dbb646c855860253d0cce7e5101627225a3b31e
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-06 20:33:06 +0200

    dodane kolo

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -1,7 +1,6 @@
-# kwadrat
-a = 10
-obwód  = a * 4
-pole = a * a
-# obrod kawadratu wynosi 40, a pole 100 
- print ("obwod kwadratu wynosi" + str(obwód) + ", pole" + str(pole)+".")
- print(pole)
+
+#koło
+pi = 3,14
+r = 5
+obwod  = 2 * pi * r
+pole = pi * r * r
