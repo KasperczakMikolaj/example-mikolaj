1e178f0782c5d8c6f0cbf4869bbd2149379cc349
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-17 22:00:46 +0200

    Zadanie 1.9

--- solo-work/solo1.py
+++ solo-work/solo1.py
@@ -65,3 +65,17 @@ posortowani_studenci = sorted(studenci, key=lambda x: x.split()[-1])
 print("Alfabetyczna lista studentow wynosi: ")
 for student in posortowani_studenci:
     print(student)
+
+
+
+# zadanie 1.9
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+
+liczba_n = 0
+for student in studenci:
+    nazwisko = student.split()[-1] 
+    if nazwisko.startswith('N'):
+        liczba_n += 1
+
+print("Liczba studentow na N wynosi:", liczba_n)
