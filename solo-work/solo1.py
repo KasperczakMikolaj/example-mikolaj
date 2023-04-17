ae941ca4420eefec9cbfbf1e088a9d3a6e8fb005
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-17 21:53:55 +0200

    Zadanie 1.3

--- solo-work/solo1.py
+++ solo-work/solo1.py
@@ -7,3 +7,10 @@ print("{} {}".format(hello, student))
 # zadanie 1.2
 student = input("Wpisz swoje imie: ")
 print("Hello {}".format(student))
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi:", liczba_studentow)
