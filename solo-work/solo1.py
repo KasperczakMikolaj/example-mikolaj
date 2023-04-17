d180ca772112f9ce680f3caa4b5777236abffb03
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-04-17 21:58:15 +0200

    Zadanie 1.7

--- solo-work/solo1.py
+++ solo-work/solo1.py
@@ -43,3 +43,13 @@ for znak in ciag_znakow:
         liczba_nawiasow_otwierajacych += 1
 
 print("Liczba nawiasow otwierajacych wynosi:", liczba_nawiasow_otwierajacych)
+
+
+# zadanie 1.7
+
+
+studenci = ["Anna Szczesny", "Tomasz Nijaki", "Barbara Kowalska", "Jan Niezbedny"]
+studenci.sort(key=lambda x: x.split()[0]) 
+print("Alfabetyczna lista studentow wynosi: ")
+for student in studenci:
+    print(student)
