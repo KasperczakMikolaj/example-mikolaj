2e16a5e0315d1173407cd062a5920fa41286575b
Mikołaj Kasperczak <73179@student.ue.poznan.pl>
2023-05-15 13:00:14 +0200

    Samochody_zadanie

--- solo-work/solo2.py
+++ solo-work/solo2.py
@@ -33,4 +33,54 @@ class Kwadrat:
 kwadrat_mikolaja = Kwadrat(12)
 print(kwadrat_mikolaja.obwod())
 print(kwadrat_mikolaja.pole())
+
+print("========================================================")
+
+class Student:
+    def __init__ (self, imie, nazwisko, numer_indeksu):
+        self.imie = imie
+        self.nazwisko = nazwisko
+        self.numer_indeksu = numer_indeksu
+        self.oceny = []
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko} {self.numer_indeksu}"
     
+    def __int__(self):
+        return 5
+    
+    def dodaj_ocene(self, ocena):
+        self.oceny.append(ocena)
+
+    def zwroc_srednia(self):
+        return sum(self.oceny) / len(self.oceny)
+    
+student_mikolaj = Student("Mikołaj", "Kasperczak","121119")
+student_mikolaj.dodaj_ocene(4.5)
+student_mikolaj.dodaj_ocene(5)
+print(student_mikolaj.oceny)
+print(student_mikolaj.zwroc_srednia())
+
+
+print("========================================================")
+
+class Samochody:
+    def __init__(self, marka, model, typ_nadwozia, silnik, kolor, wersja_wyposazenia, rocznik):
+        self.marka = marka
+        self.model = model
+        self.typ_nadwozia = typ_nadwozia
+        self.silnik = silnik
+        self.kolor = kolor
+        self.wersja_wyposazenia = wersja_wyposazenia
+        self.rocznik = rocznik
+
+    def __str__(self):
+        return f"{self.marka} {self.model} {self.typ_nadwozia} {self.silnik} {self.kolor} {self.wersja_wyposazenia} {self.rocznik}"
+    
+    def __int__(self):
+        return 10
+    
+samochod_moj = Samochody("Ford", "Mondeo", "Hatchback", "1.6_benzyna", "czarny", "Ghia", "2014")
+print(samochod_moj)
+
+    
+
